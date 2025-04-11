b60d6f28e127db2db870fea276a25257f72f2567
kvasssr <EvaSilk.ES08@gmail.com>
2025-04-11 10:03:26 +0700

    2nd part added (db mistake here)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, abort
 from data import db_session
 from data.users import User
 from data.dreams import Dreams
 from forms.user import RegisterForm, LoginForm
-from flask_login import LoginManager, login_user
+from forms.dreams import DreamsForm
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -22,8 +23,12 @@ def main():
     @app.route("/")
     def index():
         db_sess = db_session.create_session()
-        news = db_sess.query(Dreams).filter(Dreams.is_private != True)
-        return render_template("index.html", news=news)
+        if current_user.is_authenticated:
+            dreams = db_sess.query(Dreams).filter(
+                (Dreams.user == current_user) | (Dreams.is_private != True))
+        else:
+            dreams = db_sess.query(Dreams).filter(Dreams.is_private != True)
+        return render_template("index.html", dreams=dreams)
 
     @app.route('/register', methods=['GET', 'POST'])
     def reqister():
@@ -61,6 +66,76 @@ def main():
                                    message="Неправильный логин или пароль",
                                    form=form)
         return render_template('login.html', title='Авторизация', form=form)
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return redirect("/")
+
+    @app.route('/dreams', methods=['GET', 'POST'])
+    @login_required
+    def add_dreams():
+        form = DreamsForm()
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            dreams = Dreams()
+            dreams.title = form.title.data
+            dreams.content = form.content.data
+            dreams.is_private = form.is_private.data
+            current_user.news.append(dreams)
+            db_sess.merge(current_user)
+            db_sess.commit()
+            return redirect('/')
+        return render_template('dreams.html', title='Добавление сна',
+                               form=form)
+
+    @app.route('/dreams/<int:id>', methods=['GET', 'POST'])
+    @login_required
+    def edit_dreams(id):
+        form = DreamsForm()
+        if request.method == "GET":
+            db_sess = db_session.create_session()
+            dreams = db_sess.query(Dreams).filter(Dreams.id == id,
+                                              Dreams.user == current_user
+                                              ).first()
+            if dreams:
+                form.title.data = dreams.title
+                form.content.data = dreams.content
+                form.is_private.data = dreams.is_private
+            else:
+                abort(404)
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            dreams = db_sess.query(Dreams).filter(Dreams.id == id,
+                                              Dreams.user == current_user
+                                              ).first()
+            if dreams:
+                dreams.title = form.title.data
+                dreams.content = form.content.data
+                dreams.is_private = form.is_private.data
+                db_sess.commit()
+                return redirect('/')
+            else:
+                abort(404)
+        return render_template('dreams.html',
+                               title='Редактирование сна',
+                               form=form
+                               )
+
+    @app.route('/dreams_delete/<int:id>', methods=['GET', 'POST'])
+    @login_required
+    def news_delete(id):
+        db_sess = db_session.create_session()
+        dreams = db_sess.query(Dreams).filter(Dreams.id == id,
+                                          Dreams.user == current_user
+                                          ).first()
+        if dreams:
+            db_sess.delete(dreams)
+            db_sess.commit()
+        else:
+            abort(404)
+        return redirect('/')
 
     app.run()
 
